b5577b51b1ead34e30efd26532f820b3c18310f2
Rusty Rain <y@liu.mx>
2022-09-09 13:59:26 -0700

    Update main.go

--- main.go
+++ main.go
@@ -11,15 +11,14 @@ import (
 	"ngrtc/collider"
 )
 
-var tls = flag.Bool("tls", false, "whether TLS is used")
 var port = flag.Int("port", 8080, "The TCP port that the server listens on")
-var roomSrv = flag.String("room-server", "127.0.0.1", "The origin of the room server")
+var host = flag.String("host", "127.0.0.1", "The origin of the room server")
 
 func main() {
 	flag.Parse()
 
-	log.Printf("Starting collider: tls = %t, port = %d, room-server=%s", *tls, *port, *roomSrv)
+	log.Printf("Starting collider at %s:%d", *host, *port)
 
-	c := collider.NewCollider(*roomSrv)
-	c.Run(*port, *tls)
+	c := collider.NewCollider(*host)
+	c.Run(*port, false)
 }
